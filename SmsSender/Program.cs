9deb88c51037560ea00be42cc4075720789b7542
Rasim Yılmaz <iletisim@rasimyilmaz.com>
2021-06-06 11:06:21 +0300

    Selenium driverı eklendi deneme yapıldı

--- SmsSender/Program.cs
+++ SmsSender/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +12,16 @@ namespace SmsSender
     {
         static void Main(string[] args)
         {
+            IWebDriver driver;
+            driver = new ChromeDriver();
+            driver.Url = "https://www.google.com";
+            IWebElement element = driver.FindElement(By.XPath("/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input"));
+            element.SendKeys("Hello");
+            element.Clear();
+            element.SendKeys("Hi");
+
+            driver.Close();
+            driver.Quit();
 
         }
     }
